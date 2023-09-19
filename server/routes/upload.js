fdee40386e5293e24324af6aebb5c8ce82ece260
LA-248 <luca728@proton.me>
2023-09-19 23:16:53 +0200

    Add comments

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+// Configures how uploaded files will be stored (destination and name)
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: 'uploads/',
@@ -11,10 +12,12 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+// HTTP GET request to the home page - when accessed, a specific HTML file is served
 router.get('/', (req, res) => {
   res.sendFile('C:\\Users\\lucaa\\Dropbox\\Code\\Projects\\cloud-storage\\client\\public\\index.html')
 });
 
+// Create a POST route to handle the file upload and return a success message on the upload
 router.post('/upload', upload.single('file'), (req, res) => {
   res.send('File uploaded successfully!');
 });
