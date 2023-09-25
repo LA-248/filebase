9e6e443e02e9b4453c0113770b0e4df91f6ee8ca
LA-248 <luca728@proton.me>
2023-09-25 23:58:27 +0200

    Uploaded files are now displayed in the UI

--- client/public/main.js
+++ client/public/main.js
@@ -1,6 +1,9 @@
 import { openFilePicker, submitFile } from './modules/file-upload.js';
 import getFiles from './modules/fetch-files.js';
+import displayFiles from './modules/display-files.js';
 
 openFilePicker();
 submitFile();
+
 getFiles();
+displayFiles();
---
+++ client/public/modules/display-files.js
@@ -0,0 +1,19 @@
+import getFiles from './fetch-files.js';
+
+export default async function displayFiles() {
+  try {
+    const uploadedFiles = await getFiles();
+    const allUploadedFiles = document.querySelector('.uploaded-files-container');
+    console.log(uploadedFiles);
+
+    uploadedFiles.forEach(file => {
+      const uploadedFile = document.createElement('div');
+      uploadedFile.className = 'uploaded-file';
+      uploadedFile.textContent = file;
+      allUploadedFiles.append(uploadedFile);
+    });
+
+  } catch (error) {
+    console.error("An error occurred: ", error);
+  }
+}
