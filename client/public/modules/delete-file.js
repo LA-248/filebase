62a40e42f969662a3d3e81cf063ab956a149ecbe
LA-248 <luca728@proton.me>
2023-10-08 23:17:45 +0200

    Feat - Add ability to delete uploaded files

--- client/public/modules/delete-file.js
+++ client/public/modules/delete-file.js
@@ -1,15 +1,29 @@
 // Delete the respective file when button is clicked
 export default function deleteFile() {
-  const deleteButton = document.querySelector('.delete-button');
+  document.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('delete-button')) {
+      const deleteButton = event.target;
+      const fileContainer = event.target.parentElement;
+      const fileName = event.target.previousElementSibling.textContent;
 
-  deleteButton.addEventListener('click', async () => {
-    try {
-      // Send DELETE request to the specified endpoint
-      await fetch('http://localhost:3000/deleteFile', {
-      method: 'DELETE',
-    });
-    } catch (error) {
-      console.error('Error:', error.message);
+      try {
+        // Send DELETE request to the specified endpoint
+        const response = await fetch(`http://localhost:3000/deleteFile/${fileName}`, {
+            method: 'DELETE',
+          }
+        );
+        
+        deleteButton.remove();
+        fileContainer.remove();
+
+        if (response.ok) {
+          console.log(await response.json());
+        } else {
+          console.error(await response.json());
+        }
+      } catch (error) {
+        console.error('Error:', error.message);
+      }
     }
   });
 }
--- server/routes/delete-file-handler.mjs
+++ server/routes/delete-file-handler.mjs
@@ -1,16 +1,19 @@
 import express from 'express';
+import path from 'path';
 import { promises as fs } from 'node:fs';
 
 const router = express.Router();
 
 // Delete an uploaded file
-router.delete('/deleteFile', async (req, res) => {
+router.delete('/deleteFile/:filename', async (req, res) => {
   try {
-    // Remove the file specified
-    await fs.unlink('C:/Users/lucaa/Dropbox/Code/Projects/cloud-storage/server/uploads/windows-11.jpg');
-    res.status(200).send('The file was successfully deleted');
+    const filePath = path.join(`C:/Users/lucaa/Dropbox/Code/Projects/cloud-storage/server/uploads/${req.params.filename}`);
+    // Remove the file at the specified path
+    await fs.unlink(filePath);
+    res.status(200).json(`File ${req.params.filename} was successfully deleted`);
   } catch (error) {
     console.error(error);
+    res.status(500).json('Internal Server Error');
   }
 });
 
