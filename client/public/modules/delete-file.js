f49d263bcfec9c0d968bb4d7d6269e4df15adcc3
LA-248 <luca728@proton.me>
2023-10-08 23:41:23 +0200

    Refactor

--- client/public/modules/delete-file.js
+++ client/public/modules/delete-file.js
@@ -2,21 +2,25 @@
 export default function deleteFile() {
   document.addEventListener('click', async (event) => {
     if (event.target.classList.contains('delete-button')) {
+      // Retrieve relevant elements from the DOM
       const deleteButton = event.target;
       const fileContainer = event.target.parentElement;
       const fileName = event.target.previousElementSibling.textContent;
+      // Encode file name in case it contains special characters
+      const encodedFileName = encodeURIComponent(fileName);
 
       try {
-        // Send DELETE request to the specified endpoint
-        const response = await fetch(`http://localhost:3000/deleteFile/${fileName}`, {
+        // Send DELETE request to the specified endpoint with the name of the file to be deleted
+        const response = await fetch(`http://localhost:3000/deleteFile/${encodedFileName}`, {
             method: 'DELETE',
           }
         );
-        
-        deleteButton.remove();
-        fileContainer.remove();
 
         if (response.ok) {
+          // Remove the file and button from the UI if operation was successful
+          deleteButton.remove();
+          fileContainer.remove();
+
           console.log(await response.json());
         } else {
           console.error(await response.json());
--- server/routes/delete-file-handler.mjs
+++ server/routes/delete-file-handler.mjs
@@ -5,8 +5,13 @@ import { promises as fs } from 'node:fs';
 const router = express.Router();
 
 // Delete an uploaded file
+/* 
+:filename is a dynamic parameter that captures the name of the file to be deleted
+The file name is sent from the frontend when the endpoint is hit
+*/
 router.delete('/deleteFile/:filename', async (req, res) => {
   try {
+    // Retrieve the file path of the file to be deleted
     const filePath = path.join(`C:/Users/lucaa/Dropbox/Code/Projects/cloud-storage/server/uploads/${req.params.filename}`);
     // Remove the file at the specified path
     await fs.unlink(filePath);
