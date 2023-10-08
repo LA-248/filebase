5a85453cec502a88aeb5a613ac157946d4f2baa1
LA-248 <luca728@proton.me>
2023-10-08 21:41:15 +0200

    Attach a delete button to each uploaded file + assign it an ID

--- client/public/modules/display-files.js
+++ client/public/modules/display-files.js
@@ -1,5 +1,8 @@
 import fetchFiles from './fetch-files.js';
 
+// This counter is attached to the 'delete-button' HTML element and incremented each time a new file is uploaded
+let deleteButtonIdCounter = -1;
+
 export default async function displayFiles() {
   try {
     const uploadedFiles = await fetchFiles();
@@ -7,8 +10,16 @@ export default async function displayFiles() {
 
     uploadedFiles.forEach(file => {
       const uploadedFile = document.createElement('div');
+      const deleteFileButton = document.createElement('button');
+
+      deleteFileButton.className = 'delete-button';
+      deleteFileButton.textContent = 'Delete';
+      deleteFileButton.dataset.id = (deleteButtonIdCounter += 1);
+
       uploadedFile.className = 'uploaded-file';
       uploadedFile.textContent = file.fileName;
+
+      uploadedFile.append(deleteFileButton);
       allUploadedFiles.append(uploadedFile);
     });
   } catch (error) {
