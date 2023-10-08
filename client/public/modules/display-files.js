d889c03754630e01a6c73a570562a383b65da3a6
LA-248 <luca728@proton.me>
2023-10-08 23:18:39 +0200

    UI changes

--- client/public/modules/display-files.js
+++ client/public/modules/display-files.js
@@ -9,18 +9,22 @@ export default async function displayFiles() {
     const allUploadedFiles = document.querySelector('.uploaded-files-container');
 
     uploadedFiles.forEach(file => {
+      const fileContainer = document.createElement('div');
       const uploadedFile = document.createElement('div');
       const deleteFileButton = document.createElement('button');
+
+      fileContainer.className = 'file-container';
+
+      uploadedFile.className = 'uploaded-file';
+      uploadedFile.textContent = file.fileName;
 
       deleteFileButton.className = 'delete-button';
       deleteFileButton.textContent = 'Delete';
       deleteFileButton.dataset.id = (deleteButtonIdCounter += 1);
 
-      uploadedFile.className = 'uploaded-file';
-      uploadedFile.textContent = file.fileName;
-
-      uploadedFile.append(deleteFileButton);
-      allUploadedFiles.append(uploadedFile);
+      fileContainer.append(uploadedFile);
+      fileContainer.append(deleteFileButton);
+      allUploadedFiles.append(fileContainer);
     });
   } catch (error) {
     console.error('An error occurred: ', error);
