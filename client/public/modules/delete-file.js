7f9f64d2370ba418b441ddff6237605ca745885a
LA-248 <luca728@proton.me>
2023-10-09 20:43:33 +0200

    Add success response message when deleting a file to UI

--- client/public/modules/delete-file.js
+++ client/public/modules/delete-file.js
@@ -6,6 +6,7 @@ export default function deleteFile() {
       const deleteButton = event.target;
       const fileContainer = event.target.parentElement;
       const fileName = event.target.previousElementSibling.textContent;
+      const successMessage = document.querySelector('.success-message');
       // Encode file name in case it contains special characters
       const encodedFileName = encodeURIComponent(fileName);
 
@@ -15,6 +16,14 @@ export default function deleteFile() {
             method: 'DELETE',
           }
         );
+
+        const data = await response.json();
+        successMessage.textContent = data;
+        
+        // Remove success message after 4 seconds
+        setTimeout(() => {
+          successMessage.style.display = 'none';
+        }, 4000);
 
         if (response.ok) {
           // Remove the file and button from the UI if operation was successful
