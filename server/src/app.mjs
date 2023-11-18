851973267d0410dc31898a0fad8a007dc507a388
LA-248 <luca728@proton.me>
2023-11-18 19:25:36 +0100

    Clean up

--- server/src/app.mjs
+++ server/src/app.mjs
@@ -1,8 +1,6 @@
-import dotenv from 'dotenv';
-import pkg from 'express-openid-connect';
-const { auth, requiresAuth } = pkg;
-
 import express from 'express';
+import dotenv from 'dotenv';
+
 import uploads from './routes/upload.mjs';
 import getFiles from './routes/get-files.mjs';
 import deleteFile from './routes/delete-file-handler.mjs';
@@ -24,27 +22,6 @@ app.use('/', downloads);
 app.use('/', previews);
 
 app.use(express.static('../../client/public'));
-
-const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: 'process.env.SECRET_KEY',
-  baseURL: 'http://localhost:3000',
-  clientID: '5Yott16513IIFWDndeOyeFzRd0px6KWV',
-  issuerBaseURL: 'https://dev-zoq2ct5ku60c4fpk.eu.auth0.com'
-};
-
-// Auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user, null, 2));
-});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
