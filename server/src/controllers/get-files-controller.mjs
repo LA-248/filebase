9772ff5190f701bb37e55c007933cb38652e5c24
LA-248 <luca728@proton.me>
2023-12-03 18:45:44 +0100

    Rename controller files

--- server/src/controllers/get-files-controller.mjs
+++ server/src/controllers/get-files-controller.mjs
@@ -1,7 +1,9 @@
 import displayUploadedFiles from '../services/file-list.mjs';
 
 export const getStoredFiles = async (req, res) => {
+  // Retrieve the list of uploaded files
   const fileList = await displayUploadedFiles();
+  // Map the fileList into an array of objects, each with an 'id' and 'fileName'
   const indexedFileList = fileList.map((file, index) => ({
     id: index,
     fileName: file,
