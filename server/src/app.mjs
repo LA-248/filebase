ae8512e08ea582c00940e28fd740870074478fea
LA-248 <luca728@proton.me>
2023-12-04 20:53:08 +0100

    Configure auth routes

--- server/src/app.mjs
+++ server/src/app.mjs
@@ -2,15 +2,21 @@ import express from 'express';
 import session from 'express-session';
 import passport from 'passport';
 import dotenv from 'dotenv';
+import path from 'path';
 import configurePassport from './config/passport-setup.mjs';
 import { db } from '../src/services/database.mjs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 import uploads from './routes/upload.mjs';
 import getFiles from './routes/get-files.mjs';
 import deleteFile from './routes/delete-file-handler.mjs';
 import downloads from './routes/downloads.mjs';
 import previews from './routes/previews.mjs';
-import auth from './routes/auth.mjs';
+import login from './routes/login.mjs';
+import logout from './routes/logout.mjs';
 
 const app = express();
 const port = 3000;
@@ -20,7 +26,7 @@ dotenv.config();
 app.set('view engine', 'ejs');
 
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'w0OgwRgCe35EL8BYFIdAQq3N49y56D4svK90CCx3GbHF7dCDOv',
   resave: false,
   saveUninitialized: false,
 }));
@@ -35,9 +41,14 @@ app.use('/', getFiles);
 app.use('/', deleteFile);
 app.use('/', downloads);
 app.use('/', previews);
-app.use('/', auth);
+app.use('/', login);
+app.use('/', logout);
 
 app.use(express.static('../../client/public'));
+
+app.get('/login', (req, res) => {
+  res.render('login.ejs');
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
