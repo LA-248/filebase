550f845f287a117ec1fabc3222637fa24a5e9b01
LA-248 <luca728@proton.me>
2024-01-21 22:41:49 +0100

    Fix - When deleting a folder, the files in that folder were not being deleted from the database

--- server/src/controllers/delete-folder-controller.mjs
+++ server/src/controllers/delete-folder-controller.mjs
@@ -1,14 +1,21 @@
 import { db } from '../services/database.mjs';
 
 export const deleteFolder = (req, res) => {
-  const query = 'DELETE FROM folders AS f WHERE f.folderName = ? AND f.userId = ?';
+  const deleteFolder = 'DELETE FROM folders AS f WHERE f.folderName = ? AND f.userId = ?';
+  const deleteFolderContents = 'DELETE FROM files AS f WHERE f.folderName = ? AND f.userId = ?';
 
-  db.get(query, [req.params.foldername, req.user.id], err => {
+  db.get(deleteFolder, [req.params.foldername, req.user.id], err => {
     if (err) {
       res.status(500).json('Database error.');
-    } else {
-      res.status(200).json('Folder was successfully deleted.');
-      console.log(`Folder ${req.params.foldername} was successfully deleted`);
     }
+
+    db.get(deleteFolderContents, [req.params.foldername, req.user.id], err => {
+      if (err) {
+        res.status(500).json('Database error.');
+      } else {
+        res.status(200).json('Folder was successfully deleted.');
+        console.log(`Folder ${req.params.foldername} was successfully deleted.`);
+      }
+    });
   });
 };
