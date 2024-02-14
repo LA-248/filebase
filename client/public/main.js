037d3d7079f1b20a69c39a8bc0c1139b3bd81be1
LA-248 <luca728@proton.me>
2024-02-14 19:54:45 +0100

    Add search functionality

--- client/public/main.js
+++ client/public/main.js
@@ -1,19 +1,26 @@
 import { openFilePicker, openFolderPicker, submitFile, submitFolder } from './modules/file-management/upload.js';
-import { confirmFileDelete, deleteFile } from './modules/file-management/delete-file.js';
+// import { deleteFile } from './modules/file-management/delete-file.js';
 import { confirmFolderDelete, deleteFolder } from './modules/folder-management/delete-folder.js';
 import trackCurrentFolder from './utilities/session-folder-manager.js';
 import handleFileFavourites from './modules/file-management/favourites-handler.js';
+import searchFiles from './utilities/file-search.js';
 
 openFilePicker();
 openFolderPicker();
 submitFile();
 submitFolder();
 
-confirmFileDelete();
-deleteFile();
+// confirmFileDelete();
+// deleteFile();
 
 confirmFolderDelete();
 deleteFolder();
+
+searchFiles();
+const fileSearch = document.getElementById('search-files-input');
+fileSearch.addEventListener('keyup', () => {
+  searchFiles();    
+});
 
 handleFileFavourites();
 
---
+++ client/public/utilities/file-search.js
@@ -0,0 +1,39 @@
+export default function searchFiles() {
+  const searchBar = document.getElementById('search-files-input')
+    .value.toUpperCase();
+  const uploadedFileContainers = document.querySelectorAll('.file-container');
+  const uploadedFolderContainers = document.querySelectorAll('.folder-container');
+
+  for (let i = 0; i < uploadedFileContainers.length; i++) {
+    // Find the anchor tag within the file-item class
+    const fileAnchor =
+      uploadedFileContainers[i].querySelector('a.uploaded-file');
+
+    if (fileAnchor) {
+      const textValue = fileAnchor.textContent || fileAnchor.innerText;
+
+      if (textValue.toUpperCase().indexOf(searchBar) > -1) {
+        // Show the file-item
+        uploadedFileContainers[i].style.display = '';
+      } else {
+        // Hide the file-item
+        uploadedFileContainers[i].style.display = 'none';
+      }
+    }
+  }
+
+  for (let i = 0; i < uploadedFolderContainers.length; i++) {
+    const fileAnchor =
+      uploadedFolderContainers[i].querySelector('a.uploaded-folder');
+
+    if (fileAnchor) {
+      const textValue = fileAnchor.textContent || fileAnchor.innerText;
+
+      if (textValue.toUpperCase().indexOf(searchBar) > -1) {
+        uploadedFolderContainers[i].style.display = '';
+      } else {
+        uploadedFolderContainers[i].style.display = 'none';
+      }
+    }
+  }
+}
