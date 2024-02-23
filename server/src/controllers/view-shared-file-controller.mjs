8afb70237e199467fc5ab3017af88c969651e286
LA-248 <luca728@proton.me>
2024-02-23 19:03:10 +0100

    Fix - Viewing shared files not working after migrating file storage to S3

--- server/src/controllers/view-shared-file-controller.mjs
+++ server/src/controllers/view-shared-file-controller.mjs
@@ -1,55 +1,49 @@
 import path from 'path';
 import { db } from '../services/database.mjs';
+import { getPresignedUrl } from '../services/get-presigned-aws-url.mjs';
 
-// Handle the viewing of shared files
+// Handle file previews for multiple file formats
 export const viewSharedFile = (req, res) => {
-  const query = 'SELECT * FROM files AS f WHERE f.uuid = ?';
+  try {
+    const query = 'SELECT * FROM files AS f WHERE f.uuid = ?';
 
-  db.get(query, [req.params.uuid], (err, rows) => {
-    const fileName = rows.fileName;
-    const extension = path.extname(fileName);
-    console.log(fileName);
-    console.log(extension);
+    db.get(query, [req.params.uuid], async (err, rows) => {
+      if (err) {
+        console.error(`Database error: ${err.message}`);
+        res.status(500).send('An unexpected error occurred.');
+      }
 
-    if (err) {
-      console.error('Database error:', err.message);
-      res.status(500).send('An unexpected error occurred.');
-    } else if (extension === '.pdf') {
-      res.setHeader('Content-Type', 'application/pdf');
-      res.send(rows.fileData);
-    } else if (extension === '.txt') {
-      let textContent = rows.fileData.toString();
+      const fileName = rows.fileName;
+      const extension = path.extname(fileName);
+      console.log(fileName);
+      console.log(extension);
 
-      // Remove whitespace from both ends of the string
-      textContent = textContent.trim();
+      const fileData = await getPresignedUrl(process.env.BUCKET_NAME, fileName, 3600);
 
-      // Render the shared text file
-      res.render('view-shared-file.ejs', {
-        fileName: fileName,
-        folderName: rows.folderName,
-        textFilePreview: textContent,
-        fileData: null,
-      });
-    } else if (extension === '.mp4') {
-      res.setHeader('Content-Type', 'video/mp4');
-      res.send(rows.fileData);
-    } else if (extension === '.mp3') {
-      res.setHeader('Content-Type', 'audio/mp3');
-      res.send(rows.fileData);
-    } else {
-      // Create a data URL from the file buffer
-      // Convert file buffer to a base64 string for rendering
-      const dataUrl = `data:image/jpeg;base64,${rows.fileData.toString(
-        'base64'
-      )}`;
-
-      // Render the shared file
-      res.render('view-shared-file.ejs', {
-        fileName: fileName,
-        folderName: rows.folderName,
-        textFilePreview: null,
-        fileData: dataUrl,
-      });
-    }
-  });
+      // If the file is a PDF, the browser will automatically display it using the built-in PDF viewer
+      if (extension === '.pdf') {
+        res.setHeader('Content-Type', 'application/pdf');
+        res.send(fileData);
+      } else if (extension === '.txt') {
+        res.json({ url: fileData });
+      } else if (extension === '.mp4') {
+        res.setHeader('Content-Type', 'video/mp4');
+        res.send(fileData);
+      } else if (extension === '.mpeg') {
+        res.setHeader('Content-Type', 'audio/mpeg');
+        res.send(fileData);
+      } else {
+        // Render the preview in a separate page
+        res.render('view-shared-file.ejs', {
+          fileName: fileName,
+          folderName: rows.folderName,
+          textFilePreview: null,
+          fileData: fileData,
+        });
+      }
+    });
+  } catch (error) {
+    console.error(`An error occurred: ${error.message}`);
+    res.status(500).send('An error occurred while trying to preview the file.');
+  }
 };
