1913994eb397f50375dd75d760e70a2d9131ec92
Luca <luca728@proton.me>
2024-04-17 12:24:10 +0200

    Streamline code used for downloads

--- client/public/modules/file-management/download.js
+++ client/public/modules/file-management/download.js
@@ -1,8 +1,16 @@
 export default function downloadFile() {
   document.addEventListener('click', async (event) => {
     if (event.target.classList.contains('download-file-button')) {
+      let fileName;
+      const currentPath = window.location.pathname;
       const downloadButton = event.target;
-      const fileName = downloadButton.closest('.file-container').querySelector('.uploaded-file').textContent;
+
+      // File name needs to be retrieved differently from the DOM depending on which page the user is on
+      if (currentPath.includes('/home') || currentPath.includes('/favourites') || currentPath.includes('/folders')) {
+        fileName = downloadButton.closest('.file-container').querySelector('.uploaded-file').textContent;
+      } else if (currentPath.includes('/files')) {
+        fileName = document.querySelector('.file-name').textContent;
+      }
 
       try {
         const response = await fetch(`/files/${fileName}/download`, {
@@ -30,3 +38,5 @@ export default function downloadFile() {
     }
   });
 }
+
+downloadFile();
